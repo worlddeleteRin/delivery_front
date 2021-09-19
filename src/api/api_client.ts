9de4bbcde415c00f54ab27ce7c6bd77f4ef54a28
worlddeleteRin <worlddelete0@gmail.com>
2021-09-19 18:50:01 +0300

    :sparkles: add api security

--- src/api/api_client.ts
+++ src/api/api_client.ts
@@ -4,7 +4,8 @@ import axios, { AxiosInstance } from "axios";
 const apiClient: AxiosInstance = axios.create({
 	baseURL: api_url,
 	headers: {
-	"Content-type": "application/json",
+		"Content-type": "application/json",
+		"app_token": "some_access_token_is_here",
 	},
 });
 
