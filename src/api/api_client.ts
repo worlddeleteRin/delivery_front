1d3764448580bcde49fae155a7133652dc0ef52c
worlddeleteRin <worlddelete0@gmail.com>
2022-01-05 23:56:46 +0300

    :sparkles:

--- src/api/api_client.ts
+++ src/api/api_client.ts
@@ -1,12 +1,12 @@
-import { api_url } from '@/settings.ts';
+import { api_url } from "@/settings.ts";
 import axios, { AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-	baseURL: api_url,
-	headers: {
-		"Content-type": "application/json",
-		"App-Token": "some_access_token_is_here",
-	},
+  baseURL: api_url,
+  headers: {
+    "Content-type": "application/json",
+    "App-Token": "some_access_token_is_here",
+  },
 });
 
 export default apiClient;
